2f2438a609f20aab05111f32e13ad1f03331a7b6
SanG <dinhquocsang2003@gmail.com>
2024-06-11 23:31:09 +0700

    done

---
+++ ManageHotel/Repository/IRoomTypeRepository.cs
@@ -0,0 +1,17 @@
+﻿using DinhQuocSangWPF.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinhQuocSangWPF.Repository
+{
+    internal interface IRoomTypeRepository
+    {
+        void Add(RoomType roomType);
+        void Update(RoomType roomType);
+        RoomType GetById(int roomTypeId);
+        IEnumerable<RoomType> GetAll();
+    }
+}
